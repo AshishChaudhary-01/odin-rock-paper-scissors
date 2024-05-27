f1023626c8c501d35fa6744990981b1887a9f66d
MAD-GOD01 <ac9949044@gmail.com>
2024-05-27 19:26:33 +0545

    Add getHumanChoice function
    Add getHumanChoice function to get Players' choice

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-// function getComputerChoice(){
-//     randomNumber = getRandomNumber();
-//     if
-//     return computerChoice;
-// }
+function getComputerChoice(){
+    let computerChoice;
+    randomNumber = getRandomNumber();
+    if(randomNumber == 1)
+        computerChoice= "ROCK";
+    if(randomNumber == 2)
+        computerChoice = "PAPER";
+    if(randomNumber == 3)
+        computerChoice = "SCISSOR";
+    return computerChoice;
+}
 function getRandomNumber(){
     let randomNumber =Math.floor(Math.random()*3)+1;
     return randomNumber;
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your choice! Rock | Paper | Scissor ");
+    if(humanChoice.trim() === ""){
+        console.log("Input Null! Try Again!");
+        return getHumanChoice();
+    }else{
+        humanChoice = humanChoice.toUpperCase();
+        if(humanChoice!=="ROCK" && humanChoice!=="PAPER" && humanChoice!=="SCISSOR"){
+            console.log("Invalid Input! Please Try Again!");
+            return getHumanChoice();
+        }else{
+            return humanChoice;
+        }
+    }
 }
