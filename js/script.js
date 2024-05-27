e1c939b0fbafd6aad41f35c176f2c65fe82b8053
MAD-GOD01 <ac9949044@gmail.com>
2024-05-27 18:44:14 +0545

    Add getComputerChoice function
    Add a function called getComputerChoice to get the computers' random
    choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+// function getComputerChoice(){
+//     randomNumber = getRandomNumber();
+//     if
+//     return computerChoice;
+// }
+function getRandomNumber(){
+    let randomNumber =Math.floor(Math.random()*3)+1;
+    return randomNumber;
+}
